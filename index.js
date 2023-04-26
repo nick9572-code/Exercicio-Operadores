a4e173f42f4fbcffa271d60e9ed3e276bc45ef72
micaelzin <wendellmicael773@gmail.com>
2023-04-26 17:13:11 -0300

    exercicio

---
+++ index.js
@@ -0,0 +1,35 @@
+//const bool1 = true
+//const bool2 = false
+//const bool3 = !bool2
+
+//let resultado = bool1 && bool2
+//console.log("a. ", resultado)
+
+//resultado = bool1 && bool2 && bool3 
+//console.log("b. ", resultado) 
+
+//resultado = !resultado && (bool1 || bool2) 
+//console.log("c. ", resultado)
+
+
+//console.log("d. ", typeof resultado)
+//let primeiroNumero = Number(prompt("Digite um numero!"))
+//let segundoNumero = Number(prompt("Digite outro numero!"))
+
+//const soma = primeiroNumero + segundoNumero
+
+//let idade = Number (prompt("Informe a idade"))
+//let idade2 = Number(prompt("Informe a idade do seu melhor amigo/a"))
+//let maioridade idade >= idade2
+//let diferencaIdade = idade - idade2
+//console.log(maioridade)
+//console.log(diferencaIdade);
+
+
+//const idade = Number(prompt("digitesua idade"))
+
+//const idadeemMeses = * 12
+//const idadeemDias = * 345
+//const idadeemHoras = 345 * 24
+
+
